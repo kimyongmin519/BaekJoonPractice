43e4e30330a490484475cae1988a680f4121af3c
kimyongmin519 <qwdfbns5198@gmail.com>
2025-06-29 11:24:20 +0900

    [Bronze V] Title: A-B, Time: 48 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cs
@@ -0,0 +1,15 @@
+namespace Baekjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+
+            Console.WriteLine(A - B);
+        }
+    }
+}
