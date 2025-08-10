f2d162f23df3085fd5d33970c076576a4525bff5
kimyongmin519 <qwdfbns5198@gmail.com>
2025-08-10 22:17:41 +0900

    [Bronze V] Title: 오늘 날짜, Time: 48 ms, Memory: 5540 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cs
@@ -0,0 +1,12 @@
+using System.Threading.Channels;
+
+namespace Baekjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd"));
+        }
+    }
+}
