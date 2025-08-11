18fbede4a04d3c48819622d2af92237983cd777b
kimyongmin519 <qwdfbns5198@gmail.com>
2025-08-11 22:46:08 +0900

    [Bronze V] Title: 새싹, Time: 44 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,17 @@
+using System.Threading.Channels;
+
+namespace Baekjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("         ,r'\"7");
+            Console.WriteLine("r`-_   ,'  ,/");
+            Console.WriteLine(" \\. \". L_r\'");
+            Console.WriteLine("   `~\\/");
+            Console.WriteLine("      |");
+            Console.WriteLine("      |");
+        }
+    }
+}
