cc16664ed9d25c099e79772e5398762fa1691e39
kimyongmin519 <qwdfbns5198@gmail.com>
2025-11-10 12:16:45 +0900

    [Bronze II] Title: 단어의 개수, Time: 92 ms, Memory: 31196 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cs
@@ -0,0 +1,13 @@
+namespace Baekjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = Console.ReadLine();
+            string[] tralralrarotraltralra = sentence.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine(tralralrarotraltralra.Length);
+        }
+    }
+}
