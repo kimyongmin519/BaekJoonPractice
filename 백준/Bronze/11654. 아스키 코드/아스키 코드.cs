f773569d01c24514c7c8521e6641d94314ccc27f
kimyongmin519 <qwdfbns5198@gmail.com>
2025-11-10 12:33:03 +0900

    [Bronze V] Title: 아스키 코드, Time: 48 ms, Memory: 5452 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,12 @@
+namespace Baekjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char c = char.Parse(Console.ReadLine());
+
+            Console.WriteLine((int)c);
+        }
+    }
+}
