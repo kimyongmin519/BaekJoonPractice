88701a46f48e337790370342be6a4fa7b89ae566
kimyongmin519 <qwdfbns5198@gmail.com>
2025-11-10 12:33:51 +0900

    [Bronze IV] Title: $A + B - C$, Time: 48 ms, Memory: 5548 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cs
@@ -0,0 +1,16 @@
+namespace Baekjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a + b - c);
+            Console.WriteLine(int.Parse($"{a}{b}") - c);
+
+        }
+    }
+}
