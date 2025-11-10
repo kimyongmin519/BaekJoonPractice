e9eaad4f4cc243a1cbc289e62b8e561717b557de
kimyongmin519 <qwdfbns5198@gmail.com>
2025-11-10 12:32:37 +0900

    [Bronze V] Title: A×B, Time: 48 ms, Memory: 5504 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cs
@@ -0,0 +1,12 @@
+namespace Baekjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            Console.WriteLine(input[0] * input[1]);
+        }
+    }
+}
