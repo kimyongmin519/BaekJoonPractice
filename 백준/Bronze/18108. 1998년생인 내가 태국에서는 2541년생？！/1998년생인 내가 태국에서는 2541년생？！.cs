eb34a042b89812381ac8fe5b85ebd16c9f38bb9d
kimyongmin519 <qwdfbns5198@gmail.com>
2025-11-10 12:33:31 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 48 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,14 @@
+namespace Baekjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int BulgiYear = int.Parse(Console.ReadLine());
+            int SagiYear;
+
+            SagiYear = BulgiYear - 543;
+            Console.WriteLine(SagiYear);
+        }
+    }
+}
