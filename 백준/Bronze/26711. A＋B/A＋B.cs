17ea23ef62da745f2e66d29d5c04021f1a828c07
kimyongmin519 <qwdfbns5198@gmail.com>
2026-03-14 14:29:49 +0900

    [Bronze V] Title: A+B, Time: 52 ms, Memory: 5796 KB -BaekjoonHub

---
+++ 백준/Bronze/26711. A＋B/A＋B.cs
@@ -0,0 +1,16 @@
+using System.Numerics;
+
+namespace Baekjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger a = BigInteger.Parse(Console.ReadLine()!);
+            BigInteger b = BigInteger.Parse(Console.ReadLine()!);
+
+            Console.WriteLine(a + b);
+
+        }
+    }
+}
